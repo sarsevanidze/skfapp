5605d646657663ec6f2b6152a3ddc23122ca631f
sarsevanidze <t.sarsevanidze@gmail.com>
2022-06-22 18:36:33 +0400

    add total items

--- app/Http/Controllers/OrdersController.php
+++ app/Http/Controllers/OrdersController.php
@@ -13,45 +13,45 @@ class OrdersController extends Controller
     //
     public function all(Request $request) {
 
-//        $orders = Order::find(117279);
-//        return $orders;
+        $orders = Order::find(117279);
+        return $orders;
         $total_pages = 84;
-        for ($i = 80; $i <= 84; $i++) {
+        for ($i = 0; $i <= 1; $i++) {
             echo 'page' . $i;  echo '<br>';
             $orders = Order::paginate(100, $i);
             foreach ($orders['data'] as $data) {
-//                $customer = Customers::where('user_email', $data->billing->email)->first();
-//                if (!$customer) {
-//                    Customers::create([
-//                        'user_id' => 0,
-//                        'user_email' => $data->billing->email,
-//                        'first_name' => $data->billing->first_name,
-//                        'last_name' => $data->billing->last_name,
-//                        'billing' => json_encode($data->billing),
-//                        'shipping' => json_encode($data->shipping),
-//                        "billing_first_name" => $data->billing->first_name,
-//                        "billing_last_name" => $data->billing->last_name,
-//                        "billing_company" => $data->billing->company,
-//                        "billing_address_1" => $data->billing->address_1,
-//                        "billing_address_2" => $data->billing->address_2,
-//                        "billing_city" => $data->billing->city,
-//                        "billing_postcode" => $data->billing->postcode,
-//                        "billing_country" => $data->billing->country,
-//                        "billing_state" => $data->billing->state,
-//                        "billing_phone" => $data->billing->phone,
-//                        "billing_email" => $data->billing->email,
-//                        "shipping_first_name" => $data->shipping->first_name,
-//                        "shipping_last_name" => $data->shipping->last_name,
-//                        "shipping_company" => $data->shipping->company,
-//                        "shipping_address_1" => $data->shipping->address_1,
-//                        "shipping_address_2" => $data->shipping->address_2,
-//                        "shipping_city" => $data->shipping->city,
-//                        "shipping_postcode" => $data->shipping->postcode,
-//                        "shipping_country" => $data->shipping->country,
-//                        "shipping_state" => $data->shipping->state,
-//                        "shipping_phone" => $data->shipping->phone,
-//                    ]);
-//                }
+                $customer = Customers::where('user_email', $data->billing->email)->first();
+                if (!$customer) {
+                    Customers::create([
+                        'user_id' => 0,
+                        'user_email' => $data->billing->email,
+                        'first_name' => $data->billing->first_name,
+                        'last_name' => $data->billing->last_name,
+                        'billing' => json_encode($data->billing),
+                        'shipping' => json_encode($data->shipping),
+                        "billing_first_name" => $data->billing->first_name,
+                        "billing_last_name" => $data->billing->last_name,
+                        "billing_company" => $data->billing->company,
+                        "billing_address_1" => $data->billing->address_1,
+                        "billing_address_2" => $data->billing->address_2,
+                        "billing_city" => $data->billing->city,
+                        "billing_postcode" => $data->billing->postcode,
+                        "billing_country" => $data->billing->country,
+                        "billing_state" => $data->billing->state,
+                        "billing_phone" => $data->billing->phone,
+                        "billing_email" => $data->billing->email,
+                        "shipping_first_name" => $data->shipping->first_name,
+                        "shipping_last_name" => $data->shipping->last_name,
+                        "shipping_company" => $data->shipping->company,
+                        "shipping_address_1" => $data->shipping->address_1,
+                        "shipping_address_2" => $data->shipping->address_2,
+                        "shipping_city" => $data->shipping->city,
+                        "shipping_postcode" => $data->shipping->postcode,
+                        "shipping_country" => $data->shipping->country,
+                        "shipping_state" => $data->shipping->state,
+                        "shipping_phone" => $data->shipping->phone,
+                    ]);
+                }
                 $refund_id = 0;
                 if(!empty($data->refund)) {
                     $refund_id = $data->refund[0]->id;
@@ -77,6 +77,8 @@ class OrdersController extends Controller
                         'date_paid' => $data->date_paid,
                         'date_completed' => $data->date_completed,
                         'refund_id' => $refund_id,
+                        'refund' => json_encode($data->refund),
+                        'total_items' => json_encode($data->line_items)
                     ]);
                 } else {
                     echo 'old' . $order->order_id; echo '<br>';
